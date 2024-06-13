406c079715289268cb7e101b59341d694c070091
nevespgabriel <nevespgabriel@gmail.com>
2024-06-13 11:41:01 -0300

    condicao

--- instrumento.js
+++ instrumento.js
@@ -1,12 +1,33 @@
 console.log("Você toca algum instrumento?[s/n]");
-let inst;
+let resp;
 
 process.stdin.on("data", function(data){
-    let resp = data.toString().trim();
-    if(resp == "s"){
-        console.log("Qual instrumento vc toca?");
-        if(!inst){
-            inst = data.toString().trim();
+    let entrada = data.toString().trim().toLowerCase();
+    if(!resp){
+        resp = entrada
+        if(resp == "s" || resp == "sim"){
+            console.log("Qual instrumento você toca?");
+        } else if(resp == "n" || resp == "nao"){
+            console.log("Que pena!");
+            process.exit();
+        } else{
+            resp = undefined;
+            console.log("Resposta inválida. Digite sim ou nao.");
         }
+    } else{
+        switch(entrada){
+            case 'guitarra':
+                console.log("Aí sim rockeirão.");
+                break;
+            case 'piano':
+                console.log("Caraca como você é fino(a)");
+                break;
+            case 'bateria':
+                console.log("PA PA e PA");
+                break;
+            default:
+                console.log("Que massa, instrumento legal.");
+        }
+        process.exit()
     }
 });
